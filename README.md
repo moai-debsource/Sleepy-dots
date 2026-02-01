61906e1f72d0f173fe6a5f657e45caee8715f5f7
NoSleepKid <stacoverflow399@gmail.com>
2026-02-01 14:13:07 -0800

    updated

--- README.md
+++ README.md
@@ -1,17 +1,54 @@
-# Sleepy-dots
-### An application to install a dotfile setup for any arch linux install.
+#!/bin/bash
+set -e
 
-> [!NOTE]
-> 
-> This application is to be used on [Arch Linux](https://archlinux.org/) and it's TTY or Terminals with no configuration and or .icons directory.
+func_main(){
+echo "[INFO] Installing config files..."
 
-## Dependencies:
-```bash```
+mkdir -p "$HOME/.config"
 
-## Installation
+for dir in ./config/*; do
+name="$(basename "$dir")"
+if [ -e "$HOME/.config/$name" ]; then
+echo "[INFO] Removing existing $name config..."
+rm -rf "$HOME/.config/$name"
+fi
+cp -r "$dir" "$HOME/.config/"
+done
 
-### Run this command in your terminal or tty to install dotfiles!
+echo "[INFO] Installing pacman dependencies..."
+sudo pacman -S --needed --noconfirm \
+git base-devel kitty thunar hyprland rofi waybar swaybg \
+otf-font-awesome ttf-fira-code flatpak gdm
 
-```
-git clone https://github.com/moai-debsource/Sleepy-dots; cd Sleepy-dots; chmod +x install.sh; bash ./install.sh
-```
+if ! command -v yay >/dev/null 2>&1; then
+echo "[INFO] Installing yay..."
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si --noconfirm
+cd ..
+rm -rf yay-bin
+fi
+
+echo "[INFO] Installing AUR packages..."
+yay -S --needed --noconfirm grimblast wl-clipboard brave-bin
+
+echo "[INFO] Installing GRUB theme..."
+rm -rf ./LainGrubTheme
+git clone --depth=1 https://github.com/uiriansan/LainGrubTheme
+cd LainGrubTheme
+sudo ./install.sh
+sudo ./patch_entries.sh
+cd ..
+
+echo "[INFO] Installing cursor theme..."
+mkdir -p "$HOME/.icons"
+rm -rf "$HOME/.icons/Moga-White-Cursor"
+cp -r ./Moga-White-Cursor "$HOME/.icons/"
+
+echo "[INFO] Enabling display manager..."
+sudo systemctl enable gdm
+
+echo "[DONE] Install finished. Reboot and select Hyprland."
+}
+
+func_main
--- install.sh
+++ install.sh
@@ -1,35 +1,47 @@
 #!/bin/bash
+set -e
 
 func_main(){
-  echo "[INFO] Moving config directories in config folder.."
-  mkdir ~/.config
-  cp -r config/* ~/.config
-  echo "[INFO] Installing Dependencies.."
-  sudo pacman -S swaybg git kitty thunar hyprland rofi waybar swaybg base-devel otf-font-awesome nerd-fonts ttf-fira-code flatpak --noconfirm --needed 
-  echo -n "Do you have yay installed? (Y/N): "
-  read option
-  
-  if [[ "$option" == "y"  ]]; then
-    true
-  elif [[ "$option" == "n"  ]]; then
-    git clone https://aur.archlinux.org/yay-bin.git; cd yay-bin; makepkg -si; cd ..; rm -rf yay-bin
-    true
-  else
-    echo "Invalid option; aborting.."
-    exit
+  echo "[INFO] Installing config files..."
+
+  mkdir -p "$HOME/.config"
+  cp -r ./config/* "$HOME/.config/"
+
+  echo "[INFO] Installing pacman dependencies..."
+  sudo pacman -S --needed --noconfirm \
+    git base-devel kitty thunar hyprland rofi waybar swaybg \
+    otf-font-awesome ttf-fira-code flatpak gdm
+
+  if ! command -v yay >/dev/null 2>&1; then
+    echo "[INFO] Installing yay..."
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf yay-bin
   fi
-  
-  bash yay -S grimblast wl-clipboard brave-bin
-  echo "[INFO] Dependencies installed.."
-  echo "[INFO] Installing grub theme.."
-  git clone --depth=1 https://github.com/uiriansan/LainGrubTheme && cd LainGrubTheme && ./install.sh && ./patch_entries.sh
-  echo "[INFO] Grub theme installed.."
-  echo "[INFO] Installing cursor.."
-  mkdir -p ~/.icons
-  mv Moga-White-Cursor ~/.icons
-  echo "[INFO] Installing display manager.."
-  sudo pacman -S gdm --needed --noconfirm; sudo systemctl enable gdm
-  echo "[Installation] Finished installation! select hyprland at boot login."
+
+  echo "[INFO] Installing AUR packages..."
+  yay -S --needed --noconfirm grimblast wl-clipboard brave-bin
+
+  echo "[INFO] Installing GRUB theme..."
+  if [ -d "./LainGrubTheme" ]; then
+    rm -rf ./LainGrubTheme
+  fi
+  git clone --depth=1 https://github.com/uiriansan/LainGrubTheme
+  cd LainGrubTheme
+  sudo ./install.sh
+  sudo ./patch_entries.sh
+  cd ..
+
+  echo "[INFO] Installing cursor theme..."
+  mkdir -p "$HOME/.icons"
+  cp -r ./Moga-White-Cursor "$HOME/.icons/"
+
+  echo "[INFO] Enabling display manager..."
+  sudo systemctl enable gdm
+
+  echo "[DONE] Install finished. Select Hyprland on login."
 }
 
 func_main
